d459d05b496e09fad6435478f77b00408b6777e6
brenooliveira37 <brenooliveira37@gmail.com>
2020-07-04 22:31:03 -0300

    primeira versao front

--- agendaShow/src/pages/evento/evento.ts
+++ agendaShow/src/pages/evento/evento.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the EventoPage page.
@@ -22,4 +23,8 @@ export class EventoPage {
     console.log('ionViewDidLoad EventoPage');
   }
 
+  openHome() {
+    this.navCtrl.setRoot (HomePage);
+    this.navCtrl.popToRoot ();
+  }
 }
